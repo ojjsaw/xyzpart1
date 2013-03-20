3a30e6e2e8db697be3e906bc758e8cdac4f90c6b
Matt Weber <matthewweberj@gmail.com>
2013-03-20 18:21:14 -0400

    Coded Descriptor, Bucket, and Hashtable

--- Proj2part1/BufferManager/bufmgr/Bucket.java
+++ Proj2part1/BufferManager/bufmgr/Bucket.java
@@ -1,5 +1,32 @@
 package bufmgr;
-
+import global.*;
 public class Bucket {
-
+	
+	private PageId pageNumber;
+	private int frameNumber;
+	private Bucket nextBucket;
+	
+	public Bucket(PageId pageNumber, int frameNumber){
+		this.pageNumber = pageNumber;
+		this.frameNumber = frameNumber;
+	}
+	PageId getPageNumber(){
+		return pageNumber;
+	}
+	int getFrameNumber(){
+		return frameNumber;
+	}
+	Bucket getNextBucket(){
+		return nextBucket;
+	}
+	int setNextBucket(Bucket b){
+		nextBucket = b;
+		return 1;
+	}
+	boolean equals(Bucket b){
+		if(this.pageNumber.pid == b.getPageNumber().pid && this.frameNumber == b.getFrameNumber()){
+			return true;
+		}
+		return false;
+	}
 }
--- Proj2part1/BufferManager/bufmgr/Hashtable.java
+++ Proj2part1/BufferManager/bufmgr/Hashtable.java
@@ -1,6 +1,7 @@
 package bufmgr;
-
+import global.*;
 public class Hashtable {
+	private final static int HTSIZE = 251;
 /*A simple hash table should be used to figure out what frame a given disk page occupies. The hash table should be implemented (entirely in main memory) by using an array of pointers to lists of  <page number, frame number > pairs.
  * The array is called the directory and each list of pairs is called a bucket.
  * Given a page number, you should apply a hash function to find the directory entry pointing to the bucket that contains the frame number for this page, if the page is in the buffer pool.
@@ -9,4 +10,60 @@ public class Hashtable {
 /*The hash function must distribute values in the domain of the search field uniformly over the collection of buckets. If we have HTSIZE buckets, numbered 0 through M-1, a hash function h of the form 
 h(value) = (a*value+b) mod HTSIZE
  works well in practice. HTSIZE should be chosen to be a prime number.*/
+	public static Bucket Directory[] = new Bucket[HTSIZE];
+	
+	int getHash(int val){
+		return (7*val+61)%HTSIZE;
+	}
+	
+	int getFrameNumberFromBucket(PageId pageNumber){
+		Bucket b = Directory[getHash(pageNumber.pid)];
+		if (b == null){
+			return -1;
+		}
+		do{
+			if(pageNumber.pid == b.getPageNumber().pid){
+				return b.getFrameNumber();
+			}
+			b = b.getNextBucket();
+		}while(b.getNextBucket() != null);
+		return -1;
+	}
+	
+	int insertBucket(Bucket bucketToInsert){
+		Bucket b = Directory[getHash(bucketToInsert.getPageNumber().pid)];
+		if(b == null){
+			b = bucketToInsert;
+		}
+		else{
+			while(b.getNextBucket() != null){
+				b = b.getNextBucket();
+			}
+			b.setNextBucket(bucketToInsert);
+		}
+		return 1;
+	}
+	
+	Bucket removeBucket(Bucket bucketToRemove){
+		Bucket b = Directory[getHash(bucketToRemove.getPageNumber().pid)];
+		if (b == null){
+			return null;
+		}
+		if(b.equals(bucketToRemove)){
+			if(b.getNextBucket() == null){
+				Directory[getHash(bucketToRemove.getPageNumber().pid)] = null;
+				return bucketToRemove;
+			}
+			Directory[getHash(bucketToRemove.getPageNumber().pid)] = b.getNextBucket();
+			return bucketToRemove;
+		}
+		do{
+			if(b.getNextBucket().equals(bucketToRemove)){
+				b.setNextBucket(b.getNextBucket().getNextBucket());
+				return bucketToRemove;
+			}
+			b = b.getNextBucket();
+		}while(b.getNextBucket() != null);
+		return null;
+	}
 }
